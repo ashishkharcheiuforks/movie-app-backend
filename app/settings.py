dfa611385a99bf9e1f72b25f9437191441c3fa4c
Mücahit Nezir <mucahitnezir@gmail.com>
2019-08-30 11:53:28 +0300

    Completed postgresql configuration

--- app/settings.py
+++ app/settings.py
@@ -78,10 +78,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db('DATABASE_URL')
 }
 
 # Password validation
--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -17,5 +15,3 @@ urlpatterns = [
     # Admin panel
     path('admin/', admin.site.urls),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
