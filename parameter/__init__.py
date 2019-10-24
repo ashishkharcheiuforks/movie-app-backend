d94cb874006240bfafffd15a0076ca40e961fe34
Mücahit Nezir <mucahitnezir@gmail.com>
2019-10-24 16:46:09 +0300

    Upgraded requirements versions and improved translation files

---
+++ parameter/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'parameter.apps.ParameterConfig'
--- parameter/apps.py
+++ parameter/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class ParameterConfig(AppConfig):
     name = 'parameter'
+    verbose_name = _('Parameter')
